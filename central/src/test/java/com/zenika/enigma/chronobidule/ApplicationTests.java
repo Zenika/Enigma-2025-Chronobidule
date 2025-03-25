f5735d07c16f3d44102568ed4efd09875c3d63b7
Alexandre Carbenay <alexandre.carbenay@zenika.com>
2025-03-25 11:26:16 +0100

    chore: Initial commit

---
+++ central/src/test/java/com/zenika/enigma/chronobidule/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.enigma.chronobidule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
